376d8b85d70899975db5d0b5bf110f9c261c54f2
Rupali Singh <rupalisingh2304@gmail.com>
2023-03-05 19:18:39 +0530

    cors

--- app.js
+++ app.js
@@ -8,7 +8,12 @@ const  ErrorHandlerMw = require("./src/Middleware/error-handler.mw")
 const app = express()
 
 app.use(cors())
-app.options('*', cors());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+ });
 app.use(express.json())
 app.use(express.urlencoded())
 app.use(bodyParser.json())
