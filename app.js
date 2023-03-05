dcadaa5b6b2d02a1c2f3e405d17d9e96712959e0
Rupali Singh <rupalisingh2304@gmail.com>
2023-03-05 20:21:13 +0530

    cors

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ const app = express()
 app.use(cors())
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,HEAD,OPTIONS');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, authorization");
     next();
  });
 app.use(express.json())
